77823a103416a1303bc421d34c2ea0f30782e62c
RungeKut <23585965+RungeKut@users.noreply.github.com>
2023-05-25 10:09:35 +0300

    Update ir_NEC_remote_control.c

--- fan_control/User/Src/ir_NEC_remote_control.c
+++ fan_control/User/Src/ir_NEC_remote_control.c
@@ -1,14 +1,14 @@
 #include "ir_NEC_remote_control.h"
 
-#define OUT_ON TIM3->CCR1=0x0297
-#define OUT_OFF TIM3->CCR1=0x0000
+#define OUT_ON TIM3->CCR1=0x0297 //Зададим скважность ШиМ 35%
+#define OUT_OFF TIM3->CCR1=0x0000 //А тут скважность 0%
 
 void IrRemoteControInit(void)
 {
   HAL_TIM_Base_Start_IT(&htim3); // запуск таймера генератора
   NVIC_EnableIRQ(TIM3_IRQn); // разрешаем его прерывания
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1); // включаем первый канал таймера
-  OUT_OFF; // зададим начальную скважность ШиМ 35%
+  OUT_OFF; // Выключим выход на всякий случай
 }
 
 #define PREAMBLE 167 //Длина преамбулы для настройки приемника кондиционера
@@ -22,9 +22,9 @@ uint8_t bitMask = 1; //Бегущая маска
 uint16_t pulseCounter = 0; //Счетчик импульсов
 uint8_t isSendComplete = 0; //Флаг завершения отправки
 uint8_t isSendPreamble = 0; //Флаг завершения отправки преамбулы
-uint8_t cmdPush[byteLength] = {0x4D,0xB2,0xF9,0x06,0x0B,0xF4};
+uint8_t mesPush[byteLength] = {0x4D,0xB2,0xF9,0x06,0x0B,0xF4};
 uint8_t dataLength = 0; //Длина бита, изменяющаяся при передаче
-uint8_t isSendSleep = 0; //Разрешение передачи
+uint8_t isSendSleep = 1; //Разрешение передачи
 
 void IrRemoteControlCallback(void)
 {
@@ -61,7 +61,7 @@ void IrRemoteControlCallback(void)
     {
       if ( pulseCounter == 0 ) //Включаем генерацию
       {
-        uint8_t data = cmdPush[ptr] && bitMask; //Смотрим значение текущего бита
+        uint8_t data = mesPush[ptr] && bitMask; //Смотрим значение текущего бита
         if ( data == 0 ) //Выбираем длину
         {
           dataLength = FALSEBIT;
@@ -78,7 +78,7 @@ void IrRemoteControlCallback(void)
       }
       else if ( pulseCounter >= HALFBIT*dataLength ) //После передачи полной выбранной длинны бита завершаем его передачу
       {
-        if ( ptr < byteLength ) //Проверяем не закончилась ли команда
+        if ( ptr < byteLength ) //Проверяем не закончилось ли сообщение
         {
           if ( bitMask != 0x80 ) //Проверяем не передан ли весь байт целиком
           {
@@ -87,15 +87,22 @@ void IrRemoteControlCallback(void)
           else
           {
             bitMask = 1; //Если байт передан, ставим маску в начало
-            ptr++; //И выбираем следующий байт команды
+            ptr++; //И выбираем следующий байт сообщения
           }
         }
         else
         {
-          isSendComplete = 1; //Если команда закончилась ставим флаг завершения передачи
+          isSendComplete = 1; //Если сообщение закончилось, ставим флаг завершения передачи
+          isSendSleep = 1; //Запрещаем дальнейшую отправку
         }
       }
     }
     pulseCounter++; //Считаем импульсы (Количество перезагрузок таймера) (38кГц)
   }
+}
+
+void IrSendMessage(uint8_t message[byteLength])
+{
+  
+  isSendSleep = 0;
 }
